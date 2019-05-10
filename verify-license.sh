593661e15fd3c84c7fcca7f372b94475527ec446
Atsushi Neki <atsushin@fast.au.fujitsu.com>
2019-05-10 00:34:09 +0000

    BE-626 Change to check files to be committed
    Change-Id: I643941ca191867909610166b333ddb81b5683172
    Signed-off-by: Atsushi Neki <atsushin@fast.au.fujitsu.com>

--- verify-license.sh
+++ verify-license.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+#
+#    SPDX-License-Identifier: Apache-2.0
+#
+
 echo -e "\tChecking files with no license header ..."
 
-explorer_files=$(git ls-files)
+commit_files=$(git diff HEAD --name-only --cached)
 retval=0
 GREP_MATCH=0  # Exit status of grep command:  0=Matched,  1=Unmatched
 LICENSE_HEADER="SPDX-License-Identifier"
@@ -10,9 +14,9 @@ LICENSE_HEADER="SPDX-License-Identifier"
 # If you want to exclude some paths, add a keyword with an extended regular expression format to excluded_paths
 excluded_paths="\.ico$ \.jpg$ \.json$ \.png$ \.svg$ \.tx$ \.crt$ \.ya*ml$ \.key$ \.pem$ _sk$"
 
-for explorer_file in ${explorer_files}; do
+for commit_file in ${commit_files}; do
 
-  file -ib ${explorer_file} | grep -qE "^text/"
+  file -ib ${commit_file} | grep -qE "^text/"
   if [[ $? -eq ${GREP_MATCH} ]]; then
 
     # Only files which mime-type is 'text/*'
@@ -20,18 +24,18 @@ for explorer_file in ${explorer_files}; do
     isExclude=0
     for excluded_path in ${excluded_paths}; do
       # Exclude specific files from checking
-      echo ${explorer_file} | grep -qE "${excluded_path}"
+      echo ${commit_file} | grep -qE "${excluded_path}"
       if [[ $? -eq ${GREP_MATCH} ]]; then
-        # echo "EXCLUDED -- ${explorer_file}"
+        # echo "EXCLUDED -- ${commit_file}"
         isExclude=1
         break
       fi
     done
     if [[ ${isExclude} -eq 1 ]]; then continue; fi
 
-    grep -q ${LICENSE_HEADER} ${explorer_file}
+    git show :${commit_file} | grep -q ${LICENSE_HEADER}
     if [[ $? -ne ${GREP_MATCH} ]]; then
-      detected_files="${detected_files} ${explorer_file}"
+      detected_files="${detected_files} ${commit_file}"
       # If find at least 1 file that doesn't contain lincense header, return non-zero
       retval=1
     fi
